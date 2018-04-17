5b530e8b70f3261cedc6af627f0cca24babe4a30
Teresa Tuesday <teresa.beredo@gmail.com>
2018-04-17 16:32:44 -0600

    Added Login

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './containers/Home'
+import Login from "./containers/Login";
 import NotFound from './containers/NotFound'
 
 export default () =>
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+  
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
+    
   </Switch>;
