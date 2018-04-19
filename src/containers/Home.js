fd966793bc45feaa116a37ace3c7bf0f08a5e65e
Teresa Tuesday <teresa.beredo@gmail.com>
2018-04-18 21:13:30 -0600

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react'
+import { PageHeader, ListGroup } from 'react-bootstrap'
+import { API } from 'aws-amplify'
 import './Home.css'
 
 export default class Home extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isLoading: true,
+      notes: []
+    };
+  }
+  
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+    
+    try{
+      const notes = await this.notes()
+      this.setState({notes})
+    } catch (e) {
+      alert(e);
+    }
+    
+    this.setState ({ isLoading:false })
+  }
+  
+  notes() {
+    return API.get("notes", "/notes")
+  }
+  
+  renderNotesList(notes) {
+    return null;
+  }
+  
+  renderLander() {
     return (
-      <div className="Home">
-        <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
-        </div>
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
       </div>
     );
   }
+  
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
+  render() {
+  return (
+    <div className="Home">
+      {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
+    </div>
+    );
+  }
 }
+
